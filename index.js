7d71059c380e4df6fc0b230c54bace4949b4f658
ToniaTotty <56296521+ToniaTotty@users.noreply.github.com>
2023-02-24 02:37:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let hourEl = document.getElementById("hour") 
+let minuteEl = document.getElementById("minutes")
+let secondEl = document.getElementById("seconds")
+let timeSignatureEl = document.getElementById("ampm")
+
+updateClock = () => {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h > 12){
+        h = h - 12
+        ampm = "PM"
+    }
+
+    h = h<10 ? "0" + h : h
+    m = m<10 ? "0" + m : m
+    s = s<10 ? "0" + s : s
+
+    hourEl.innerText = h
+    minuteEl.innerText = m
+    secondEl.innerText = s
+    timeSignatureEl.innerText = ampm
+
+    setTimeout(()=>{
+        updateClock()
+    }, 1000)
+}
+updateClock()
